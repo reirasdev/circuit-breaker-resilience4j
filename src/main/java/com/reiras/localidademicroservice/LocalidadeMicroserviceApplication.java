9399fce48ab74431a3a5cc42024bb5118fed02a8
reirasdev <reiras.dev@gmail.com>
2020-05-28 13:53:06 -0300

    Initial project

---
+++ src/main/java/com/reiras/localidademicroservice/LocalidadeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.reiras.localidademicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalidadeMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalidadeMicroserviceApplication.class, args);
+	}
+
+}
