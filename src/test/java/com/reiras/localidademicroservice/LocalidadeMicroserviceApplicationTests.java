da3fb79333503ba94f48ec9648621738853b1fe9
reirasdev <reiras.dev@gmail.com>
2020-05-28 14:05:39 -0300

    Domain and DAO tests for Localidade entity

--- src/test/java/com/reiras/localidademicroservice/LocalidadeMicroserviceApplicationTests.java
+++ src/test/java/com/reiras/localidademicroservice/LocalidadeMicroserviceApplicationTests.java
@@ -1,14 +1,22 @@
 package com.reiras.localidademicroservice;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.reiras.localidademicroservice.dao.Dao;
 
 @SpringBootTest
 class LocalidadeMicroserviceApplicationTests {
 
+	@Autowired
+	private Dao jsonDao;
+
 	@Test
 	void contextLoads() {
-
+		assertNotNull(jsonDao);
 	}
 
 }
